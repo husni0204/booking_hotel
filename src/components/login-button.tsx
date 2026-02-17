eb9564daf14c6d2f65f0de58f1177e8ab5db224d
Husni Mubarok <husnirezky2@gmail.com>
2026-02-17 18:04:45 +0700

    1:36:52 install auth prisma adapter

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -1,13 +1,19 @@
+import { signIn } from "@/auth";
 import { FC } from "react";
 import { FaG } from "react-icons/fa6";
 
-const LoginButton: FC = () => {
+export const LoginGoogleButton: FC = () => {
     return (
-        <button className="flex items-center justify-center gap-2 w-full bg-blue-700 text-white font-medium py-3 px-6 text-base rounded-sm hover:bg-blue-500 cursor-pointer">
-            <FaG className="size-6" />
-            Sign In With Google
-        </button>
+        <form
+            action={async () => {
+                "use server";
+                await signIn("google");
+            }}
+        >
+            <button className="flex items-center justify-center gap-2 w-full bg-blue-700 text-white font-medium py-3 px-6 text-base rounded-sm hover:bg-blue-500 cursor-pointer">
+                <FaG className="size-6" />
+                Sign In With Google
+            </button>
+        </form>
     );
 };
-
-export default LoginButton;
